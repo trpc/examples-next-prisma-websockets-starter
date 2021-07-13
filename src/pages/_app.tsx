cfec577a4fc255b9d8e774537815d347e56ee800
Alex Johansson <alexander@n1s.se>
2021-07-13 15:14:56 +0200

    improve websockets support  (#595)

---
+++ src/pages/_app.tsx
@@ -0,0 +1,68 @@
+import { httpBatchLink } from '@trpc/client/links/httpBatchLink';
+import { wsLink, createWSClient } from '@trpc/client/links/wsLink';
+import { loggerLink } from '@trpc/client/links/loggerLink';
+import { withTRPC } from '@trpc/next';
+import { AppType } from 'next/dist/next-server/lib/utils';
+import type { AppRouter } from 'server/routers/app';
+import superjson from 'superjson';
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+function getEndingLink() {
+  const { APP_URL, WS_URL } = publicRuntimeConfig;
+  if (!process.browser) {
+    return httpBatchLink({
+      url: `${APP_URL}/api/trpc`,
+    });
+  }
+  const client = createWSClient({
+    url: WS_URL,
+  });
+  return wsLink<AppRouter>({
+    client,
+  });
+}
+
+export default withTRPC<AppRouter>({
+  config() {
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @link https://trpc.io/docs/ssr
+     */
+
+    return {
+      /**
+       * @link https://trpc.io/docs/links
+       */
+      links: [
+        // adds pretty logs to your console in development and logs errors in production
+        loggerLink({
+          enabled: (opts) =>
+            (process.env.NODE_ENV === 'development' && process.browser) ||
+            (opts.direction === 'down' && opts.result instanceof Error),
+        }),
+        getEndingLink(),
+      ],
+      /**
+       * @link https://trpc.io/docs/data-transformers
+       */
+      transformer: superjson,
+      /**
+       * @link https://react-query.tanstack.com/reference/QueryClient
+       */
+      queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+    };
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   */
+  ssr: true,
+})(MyApp);
---
+++ src/server/routers/app.ts
@@ -0,0 +1,42 @@
+/**
+ * This file contains the tRPC http response handler and context creation for Next.js
+ */
+import { Subscription } from '@trpc/server';
+import { clearInterval } from 'timers';
+import { createRouter } from '../trpc';
+import { postsRouter } from './posts';
+import superjson from 'superjson';
+// Infers the context returned from `createContext`
+
+/**
+ * Create your application's root router
+ * If you want to use SSG, you need export this
+ * @link https://trpc.io/docs/ssg
+ * @link https://trpc.io/docs/router
+ */
+export const appRouter = createRouter()
+  /**
+   * Add data transformers
+   * @link https://trpc.io/docs/data-transformers
+   */
+  .transformer(superjson)
+  /**
+   * Optionally do custom error (type safe!) formatting
+   * @link https://trpc.io/docs/error-formatting
+   */
+  // .formatError(({ shape, error }) => { })
+  .merge('posts.', postsRouter)
+  .subscription('randomNumber', {
+    resolve() {
+      return new Subscription<number>((emit) => {
+        const int = setInterval(() => {
+          emit.data(Math.random());
+        }, 500);
+        return () => {
+          clearInterval(int);
+        };
+      });
+    },
+  });
+
+export type AppRouter = typeof appRouter;
